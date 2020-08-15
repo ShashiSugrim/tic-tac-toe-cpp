de1cb88ba0b7e04add856a015fb63eb4db9a6794
Shashi Sugrim <ssugrim1430@bths.edu>
2020-08-15 19:37:30 +0000

    full game works, diagonal check added

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -17,8 +17,10 @@ public:
     }
     void printBoard()
     {
+        cout << "Column  1 2 3 \n";
         for (int x = 0; x < 3; x++)
         {
+            cout << "Row #" + to_string(x + 1) + ": ";
             for (int y = 0; y < 3; y++)
             {
                 cout << to_string(board[x][y]) + " ";
@@ -50,7 +52,7 @@ public:
         }
         return;
     }
-    string checkHorizontal(string player)
+    bool checkHorizontal(string player)
     {
         int numPlayer = 2;
         if (player == playerOne)
@@ -61,12 +63,12 @@ public:
         {
             if (((board[i][0] == board[i][1]) && (board[i][2] == board[i][0])) && (board[i][0] != 0))
             {
-                return "win horizontal";
+                return true;
             }
         }
-        return "nowin horizontal";
+        return false;
     }
-    string checkVertical(string player)
+    bool checkVertical(string player)
     {
         int numPlayer = 2;
         if (player == playerOne)
@@ -77,10 +79,27 @@ public:
         {
             if (((board[0][i] == board[1][i]) && (board[2][i] == board[0][i])) && (board[0][i] != 0))
             {
-                return "win vertical";
+                return true;
             }
         }
-        return "nowin vertical";
+        return false;
+    }
+    bool checkDiagonal(string player)
+    {
+        int numPlayer = 2;
+        if (player == playerOne)
+        {
+            numPlayer == 1;
+        }
+        if (((board[0][0] == board[1][1]) && (board[2][2] == board[0][0])) && (board[0][0] != 0))
+        {
+            return true;
+        }
+        else if (((board[0][2] == board[1][1]) && (board[2][0] == board[0][2])) && (board[1][1] != 0))
+        {
+            return true;
+        }
+        return false;
     }
 };
 
@@ -89,20 +108,54 @@ int main()
     Game mainGame;
     string playerOne;
     string playerTwo;
+    string playerTurn;
+    int colRegister;
+    int rowRegister;
     int moveNum = 1;
     bool winner = false;
     cout << "\nWelcome to Tic Tac Toe!!!";
+    cout << "\nPlayer Two will always make the first move";
+    cout << "\nPlayer One is number 1 on the board";
+    cout << "\nPlayer Two is number 2 on the board";
     cout << "\nWhat is player one's name?: ";
     cin >> playerOne;
     cout << "\nWhat is player twos's name?: ";
     cin >> playerTwo;
     mainGame.setPlayers(playerOne, playerTwo);
+
     mainGame.printBoard();
-    cout << "\nMove set by Player 1\n";
-    mainGame.setMove(playerOne, 2, 0);
-    mainGame.setMove(playerOne, 2, 1);
-    mainGame.setMove(playerOne, 2, 2);
-    mainGame.printBoard();
-    cout << "\n" + mainGame.checkHorizontal(playerOne) + "\n";
-    cout << "\n" + mainGame.checkVertical(playerOne) + "\n";
+    while (!winner)
+    {
+
+        if (moveNum % 2 == 0)
+        {
+            playerTurn = playerOne;
+            cout << "\nPlayer " + playerTurn + " is number 1 on the board";
+        }
+        else
+        {
+            playerTurn = playerTwo;
+            cout << "\nPlayer " + playerTurn + " is number 2 on the board";
+        }
+        cout << "\n";
+        cout << playerTurn + " makes move # " + to_string(moveNum);
+        cout << "\n";
+        cout << playerTurn + " choose column: ";
+        cin >> colRegister;
+        cout << "\n";
+        cout << playerTurn + " choose row: ";
+        cin >> rowRegister;
+        colRegister -= 1;
+        rowRegister -= 1;
+        mainGame.setMove(playerTurn, colRegister, rowRegister);
+        if (mainGame.checkVertical(playerTurn) || mainGame.checkHorizontal(playerTurn) || mainGame.checkDiagonal(playerTurn))
+        {
+            cout << "\n";
+            cout << playerTurn + " has won the game in " + to_string(moveNum) + " moves!!!\n";
+            winner = true;
+        }
+        mainGame.printBoard();
+        moveNum++;
+        cout << "\n-----------------------------------\n";
+    }
 }
